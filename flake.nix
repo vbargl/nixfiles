aa492e1165aafdcd31da0e7ccdf95809cf4d4cfe
Vojtěch Bargl <bargl.vojtech@gmail.com>
2025-10-12 18:47:04 +0200

    initializing flakes

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = ''
+    Nix flake for managing NixOS configurations and Home Manager setups.
+
+    This flake includes:
+    - NixOS configuration for the host "peacock"
+      - With special subfolder _modules which contains reusable modules for nixos
+    - Home Manager configuration for the user "vbargl"
+      - With special subfolder _modules which contains reusable modules for home-manager
+    - DevShells for my development environments
+  '';
+  
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixunstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    home-manager = {
+      url = "github:nix-community/home-manager?ref=release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    agenix = {
+      url = "github:ryantm/age-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = flakes: {};
+}
