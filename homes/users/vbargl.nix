af6b2ed75f320cbb3411a3b39863849557aa53cf
Vojtěch Bargl <bargl.vojtech@gmail.com>
2025-11-23 23:09:11 +0100

    nix registry as activation script

--- homes/users/vbargl.nix
+++ homes/users/vbargl.nix
@@ -1,24 +1,12 @@
-{ self, ... }:
+{ self, inputs, ... }:
 {
   nixpkgs.config = self.config.nixpkgs;
 
-  nix.registry = {
-    "vbargl".to = {
-      type = "github";
-      owner = "vbargl";
-      repo = "nixfiles";
-    };
-    "nixpkgs".to = {
-      type = "github";
-      owner = "nixos";
-      repo = "nixpkgs";
-    };
-    "unstable".to = {
-      type = "github";
-      owner = "nixos";
-      repo = "nixpkgs";
-    };
-  };
+  home.activation.setupNixRegistry = ''
+    nix registry add vbargl github:vbargl/nixfiles
+    nix registry add nixpkgs github:nixos/nixpkgs/${inputs.unstable.rev}
+    nix registry add nixos github:nixos/nixpkgs/${inputs.nixpkgs.rev}
+  '';
 
   home = {
     stateVersion = "25.05";
