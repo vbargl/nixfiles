d116c5ba4cda5c8618c5f885f931962181b2a3e1
Vojtěch Bargl <bargl.vojtech@gmail.com>
2026-04-18 14:46:34 +0200

    feat(minimal): add chaotic-nyx + nix-community substituters

--- machines/modules/minimal.nix
+++ machines/modules/minimal.nix
@@ -1,6 +1,15 @@
 {
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  nix.settings.substituters = [
+    "https://chaotic-nyx.cachix.org"
+    "https://nix-community.cachix.org"
+  ];
+  nix.settings.trusted-public-keys = [
+    "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+  ];
+
   nix.gc = {
     automatic = true;
     dates = "weekly";
