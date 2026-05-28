3eb787ba13663071e5460fc00a392e2781568271
Vojtěch Bargl <bargl.vojtech@gmail.com>
2026-05-28 22:59:20 +0200

    fix(deploy-rs): source package from unstable

--- overlays/deploy-rs.nix
+++ overlays/deploy-rs.nix
@@ -3,9 +3,9 @@
   flake.overlays.deploy-rs =
     final: _prev:
     let
-      deploy-rs = inputs.deploy-rs.packages.${final.stdenv.hostPlatform.system};
+      unstable = inputs.unstable.legacyPackages.${final.stdenv.hostPlatform.system};
     in
     {
-      deploy-rs = deploy-rs.default;
+      inherit (unstable) deploy-rs;
     };
 }
